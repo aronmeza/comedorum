73565a247794c82beb54b5365ba55a0382eb4c57
Eder Salim Martinez <edermtz.04@gmail.com>
2012-05-08 18:01:54 -0500

    sumaRecetas

--- grails-app/services/org/conversor/RecetaService.groovy
+++ grails-app/services/org/conversor/RecetaService.groovy
@@ -33,63 +33,57 @@ class RecetaService {
         
     @Transactional(readOnly = true)    
     List<Ingrediente> sumaIngredientes(List<Ingrediente> ingrs, Long idReceta, BigDecimal rendimiento){
-        println "=================> $ingrs"
-        Map<Long,Ingrediente> suma=new HashMap<Integer, BigDecimal>()
-        for(Ingrediente ing:ingrs){
-        	if(ing.unidadMedida.equals('kg')||ing.unidadMedida.equals('l')){
-                ing.cantidad=ing.cantidad.multiply(new BigDecimal("1000"))
-                if(ing.unidadMedida.equals('kg')){
-                    ing.unidadMedida='gr'
-                }else{
-                    ing.unidadMedida='ml'
-                }
-            }
-            suma.put(ing.id,ing)
-        }
-        println "==SUMA===============> $suma"
-        def receta=convertirReceta (null, rendimiento, idReceta)
-        for(Ingrediente ingres:receta.ingredientes){
-            if(ingres.unidadMedida.equals('kg')||ingres.unidadMedida.equals('l')){
-                ingres.cantidad=ingres.cantidad.multiply(new BigDecimal("1000"))
-                if(ingres.unidadMedida.equals('kg')){
-                    ingres.unidadMedida='gr'
-                }else{
-                    ingres.unidadMedida='ml'
-                }
-                
-                if(suma.containsKey(ingres.id)){
-                println "											"
-                println suma.getAt(ingres.id).cantidad.add(ingres.cantidad)
-                println suma.getAt(ingres.id).cantidad
-                println ingres.cantidad
-                    ingres.cantidad=suma.getAt(ingres.id).cantidad.add(ingres.cantidad)
-                    suma.put(ingres.id,ingres)
-                }else{
-                    suma.put(ingres.id,ingres)
-                }
-            }
-            
-        }
+       Map<Long,Ingrediente> suma=new HashMap<Integer, BigDecimal>()
+       for(Ingrediente ing:ingrs){
+           ing=transformaUnidadMedida(ing,true)
+           suma.put(ing.materia.id,ing)
+       }
+       def receta=convertirReceta (null, rendimiento, idReceta)
+       for(Ingrediente ingres:receta.ingredientes){
+           ingres=transformaUnidadMedida(ingres,true)
+           if(suma.containsKey(ingres.materia.id)){
+               ingres.cantidad=suma.getAt(ingres.materia.id).cantidad.add(ingres.cantidad)
+               suma.put(ingres.materia.id,ingres)
+           }else{
+               suma.put(ingres.materia.id,ingres)
+           }
+       }
+       
         List<Ingrediente> nueva=new ArrayList<Ingrediente>()
         def llaves=suma.keySet()
         def itera=llaves.iterator()
         while(itera.hasNext()){
-            Long id=itera.next()
+            Long id=itera.next() 
             Ingrediente ingreFinal=suma.getAt(id)
-            if (ingreFinal.unidadMedida.equals('gr') || ing.unidadMedida.equals('ml')){
-                    if(ingreFinal.cantidad/1000>=1){
-                            ingreFinal.cantidad = ingreFinal.cantidad/1000
-                            if(ingreFinal.unidadMedida == 'gr'){
-                                    ingreFinal.unidadMedida = 'kg'
-                            }else{
-                                    ingreFinal.unidadMedidad = 'l'
-                            }
-                    }
-            }
+            ingreFinal=transformaUnidadMedida(ingreFinal,false)
             nueva.add(ingreFinal)
         }
-        println "																	"
-        println ingrs
-        return nueva
+        
+        return nueva        
+
     }
+    Ingrediente transformaUnidadMedida(Ingrediente ingrediente, Boolean flag){
+        if(flag){
+          if(ingrediente.unidadMedida.equals('kg')||ingrediente.unidadMedida.equals('l')){
+            ingrediente.cantidad=ingrediente.cantidad.multiply(new BigDecimal("1000"))
+            if(ingrediente.unidadMedida.equals('kg')){
+                ingrediente.unidadMedida='gr'
+            }else{
+                ingrediente.unidadMedida='ml'
+            }
+          }            
+        }else{
+            if (ingrediente.unidadMedida.equals('gr') || ingrediente.unidadMedida.equals('ml')){
+                if(ingrediente.cantidad/1000>=1){
+                    ingrediente.cantidad = ingrediente.cantidad/1000
+                    if(ingrediente.unidadMedida == 'gr'){
+                        ingrediente.unidadMedida = 'kg'
+                    }else{
+                        ingrediente.unidadMedida = 'l'
+                    }
+                }
+            }            
+        }
+        return ingrediente
+   }
 }
